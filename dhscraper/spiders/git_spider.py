e59dd6a31f4fcce70878436d5aa4242bae080fb6
Lisa Poggel <poggellisa@gmail.com>
2023-09-25 19:25:56 +0200

    add response type handling

--- dhscraper/spiders/git_spider.py
+++ dhscraper/spiders/git_spider.py
@@ -18,8 +18,8 @@ class GitSpider(scrapy.Spider):
                   "https://api.github.com/repos/ADHO/dh2018/contents/xml/posters",
                   "https://api.github.com/repos/ADHO/dh2018/contents/xml/short-papers",
                   "https://api.github.com/repos/ADHO/dh2018/contents/xml/workshops",
+                  "https://api.github.com/repos/ADHO/dh2017/contents/pdf",
                   ]
-                    # "https://api.github.com/repos/ADHO/dh2017/contents/pdf",
 
     def parse(self, response):
         """
@@ -27,16 +27,19 @@ class GitSpider(scrapy.Spider):
         """
         response_dict = json.loads(response.body)  # .text
         for item in response_dict:
-            yield scrapy.Request(item["download_url"], callback=self.parse_node)
+            yield scrapy.Request(item["download_url"], callback=self.parse_abstract)
 
-    def parse_node(self, response): # rename?
+    def parse_abstract(self, response):
         """
         handles the response downloaded for each of the requests made: extracts links to dh projects from abstract xml files
         """
-        xml_string = response.text
-        root = ET.fromstring(xml_string)
-        refs = root.findall('.//{http://www.tei-c.org/ns/1.0}ref')
         item = DhscraperItem()
-        item["origin"] = response.url
-        item["urls"] = [ref.attrib['target'] for ref in refs]
+        if response.url.endswith(".pdf"):
+            item["origin"] = response.url
+        else:
+            xml_string = response.text
+            root = ET.fromstring(xml_string)
+            refs = root.findall('.//{http://www.tei-c.org/ns/1.0}ref')
+            item["origin"] = response.url
+            item["urls"] = [ref.attrib['target'] for ref in refs]
         return item
