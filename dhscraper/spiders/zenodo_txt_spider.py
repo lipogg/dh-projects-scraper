f80342ff47c34211781ac97d35e2d65de9664937
Lisa Poggel <poggellisa@gmail.com>
2023-10-02 19:33:08 +0200

    optimize url pattern matching for incomplete urls

--- dhscraper/spiders/zenodo_txt_spider.py
+++ dhscraper/spiders/zenodo_txt_spider.py
@@ -19,7 +19,22 @@ class ZenodoTXTSpider(scrapy.Spider):
         item["origin"] = response.url
         url_pattern = r"(https?://)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.(?:[a-zA-Z0-9()]{1,6}\b)(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)"  # match emails here and exclude later to prevent accidentally matching email domains
         abstract = response.text
-        item["urls"] = {match.group() for match in re.finditer(url_pattern, abstract)}
+        #item["urls"] = {match.group() for match in re.finditer(url_pattern, abstract)}
+        item["urls"] = set()
+        for match in re.finditer(url_pattern, abstract):
+            url = match.group()
+            if url.endswith(("-", "_")):
+                # Start position after url match
+                start_pos = match.end()
+                subsequent_text = abstract[start_pos:]
+                # Pattern for a line break followed by spaces and a URL path
+                end_url_pattern = r"[\r\n\s]+([-a-zA-Z0-9@:%._\+~#=]{1,256})"
+                end_match = re.search(end_url_pattern, subsequent_text)
+                # If there is a match, concatenate with the original URL
+                if end_match:
+                    url_end = end_match.group(1)
+                    url = url + url_end
+            item["urls"].add(url)
         item["abstract"] = "n.a."
         yield item
 
