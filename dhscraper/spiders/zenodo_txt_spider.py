009b319298faf9abbace4b7b2174fa009a165a80
Lisa Poggel <poggellisa@gmail.com>
2023-10-05 01:29:05 +0200

    refine regex for multiline urls, with dedicated logging and error handling

--- dhscraper/spiders/zenodo_txt_spider.py
+++ dhscraper/spiders/zenodo_txt_spider.py
@@ -1,7 +1,7 @@
 import scrapy
 from dhscraper.items import DhscraperItem
-import re
-
+import logging
+import regex
 
 class ZenodoTXTSpider(scrapy.Spider):
     """identify the spider"""
@@ -15,27 +15,44 @@ class ZenodoTXTSpider(scrapy.Spider):
         """
         handles the response downloaded for each of the requests made
         """
+        logging.info('Parse function called on %s', response.url)
         item = DhscraperItem()
+        logging.debug('DhscraperItem created')
         item["origin"] = response.url
-        url_pattern = r"(https?://)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.(?:[a-zA-Z0-9()]{1,6}\b)(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)"  # match emails here and exclude later to prevent accidentally matching email domains
+        url_pattern = (
+            r"(https?://)?"  # matches optional "http://" or "https://"
+            r"(www\.)?"  # matches optional "www."            
+            r"[-a-zA-Z0-9@:%._\+~#=]{1,256}"  # matches main part of the domain name 
+            r"(?:-[\r\n]{0,4}[-a-zA-Z0-9@:%._\+~#=]{1,256})?"  # matches possible line breaks and the continuation of the domain or path, "-" to exclude overmatching in cases such as via\nraganwald.com 
+            r"(?:\.[a-zA-Z0-9()]{1,6}\b)+?"   # matches the TLD and possible SLDs, non-greedy quantifier to reduce backtracking
+            r"(?:[\r\n]{0,4}[-a-zA-Z0-9()@:%_\+.~#?&//=]{1,256})?"  # matches possible line breaks and the continuation of the path or query params, anchors etc
+        )
+        logging.debug('URL pattern: %s', url_pattern)
         abstract = response.text
-        #item["urls"] = {match.group() for match in re.finditer(url_pattern, abstract)}
         item["urls"] = set()
-        for match in re.finditer(url_pattern, abstract):
-            url = match.group()
-            if url.endswith(("-", "_")):
-                # Start position after url match
+        end_url_pattern = r"[\r\n\s]+([-a-zA-Z0-9@:%._\+~#=/]{1,256})"
+        try:
+            for match in regex.finditer(url_pattern, abstract, timeout=20):
+                logging.debug('Match found: %s', match.group())
+                url = match.group()
                 start_pos = match.end()
-                subsequent_text = abstract[start_pos:]
-                # Pattern for a line break followed by spaces and a URL path
-                end_url_pattern = r"[\r\n\s]+([-a-zA-Z0-9@:%._\+~#=]{1,256})"
-                end_match = re.search(end_url_pattern, subsequent_text)
-                # If there is a match, concatenate with the original URL
-                if end_match:
-                    url_end = end_match.group(1)
-                    url = url + url_end
-            item["urls"].add(url)
+                # catch continuation of the path if split across lines, test case mw2013.museumsandtheweb.com/...
+                while url.endswith(("-", "_", "–")):
+                    logging.debug('URL ends with "-", "_", or "–": %s', url)
+                    subsequent_text = abstract[start_pos:]
+                    end_match = regex.search(end_url_pattern, subsequent_text)
+                    if end_match:
+                        logging.debug('End match found: %s', end_match.group(1))
+                        url_end = end_match.group(1)
+                        url += url_end
+                        start_pos += len(url_end)
+                    else:
+                        break
+                item["urls"].add(url)
+        except regex.TimeoutError:
+            logging.error('Regex timed out on %s', response.url)
         item["abstract"] = "n.a."
+        logging.info('Item ready to be yielded')
         yield item
 
 
