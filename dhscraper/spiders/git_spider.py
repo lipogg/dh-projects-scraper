6f92ad093de3aec6e808eae4324c56c7bc39b846
Lisa Poggel <poggellisa@gmail.com>
2023-10-05 02:37:26 +0200

    refine regex url matching and xml tree parsing; add logging and error handling

--- dhscraper/spiders/git_spider.py
+++ dhscraper/spiders/git_spider.py
@@ -2,7 +2,8 @@ import scrapy
 from dhscraper.items import DhscraperItem
 import xml.etree.ElementTree as ET
 import json
-import re
+import regex
+import logging
 
 
 class GitSpider(scrapy.Spider):
@@ -25,6 +26,7 @@ class GitSpider(scrapy.Spider):
         """
         handles the response downloaded for each of the requests made
         """
+        logging.info('Parse function called on %s', response.url)
         response_dict = json.loads(response.body)
         for item in response_dict:
             yield scrapy.Request(item["download_url"], callback=self.parse_abstract, meta={"start_url": response.url})
@@ -34,16 +36,32 @@ class GitSpider(scrapy.Spider):
         handles the response downloaded for each of the requests made: extracts links to dh projects from abstract xml files
         """
         item = DhscraperItem()
+        logging.debug('DhscraperItem created')
         item["origin"] = response.meta["start_url"]
-        pattern = r"(https?://)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.(?:[a-zA-Z0-9()]{1,6}\b)(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)"  # match emails here and exclude later to prevent accidentally matching email domains
         xml_string = response.text
         root = ET.fromstring(xml_string)
         refs = root.findall('.//{http://www.tei-c.org/ns/1.0}ref')
         item["abstract"] = response.url
-        item["urls"] = {ref.attrib['target'] for ref in refs}
+        try:
+            item["urls"] = {ref.attrib['target'] for ref in refs}
+        except KeyError:
+            logging.error('Ref element does not have a target attribute.')
         # catch malformed urls: Several urls are not <ref> element attributes, but plain text in <p> elements
-        p_text = ' '.join([p.text for p in root.findall('.//{http://www.tei-c.org/ns/1.0}p') if p.text])
-        bibl_text = ' '.join([bibl.text for bibl in root.findall('.//{http://www.tei-c.org/ns/1.0}bibl') if bibl.text])
-        mf_urls = {match.group() for match in re.finditer(pattern, f"{p_text} {bibl_text}")}
-        item["urls"].update(mf_urls)
+        url_pattern = (
+            r"(https?://)?"  # matches optional "http://" or "https://"
+            r"(www\.)?"  # matches optional "www."            
+            r"[-a-zA-Z0-9@:%._\+~#=]{1,256}"  # matches main part of the domain name 
+            r"(?:-[\r\n]{0,4}[-a-zA-Z0-9@:%._\+~#=]{1,256})?"  # matches possible line breaks and the continuation of the domain or path, "-" to exclude overmatching in cases such as via\nraganwald.com 
+            r"(?:\.[a-zA-Z0-9()]{1,6}\b)+?"  # matches the TLD and possible SLDs, non-greedy quantifier to reduce backtracking
+            r"(?:[\r\n]{0,4}[-a-zA-Z0-9()@:%_\+.~#?&//=]{1,256})?"  # matches possible line breaks and the continuation of the path or query params, anchors etc
+        )
+        logging.debug('URL pattern: %s', url_pattern)
+        body_element = root.find('.//{http://www.tei-c.org/ns/1.0}body')
+        body_text = ''.join(body_element.itertext())
+        try:
+            mf_urls = {match.group() for match in regex.finditer(url_pattern, body_text, timeout=20)} #f"{p_text} {bibl_text}"
+            logging.debug('Matches found: %s', mf_urls)
+            item["urls"].update(mf_urls)
+        except regex.TimeoutError:
+            logging.error('Regex timed out on %s', response.url)
         yield item
