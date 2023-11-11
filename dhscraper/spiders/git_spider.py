91db9bdb8a7658892423e17e84d5295ae5890c3c
Lisa Poggel <poggellisa@gmail.com>
2023-11-11 15:27:28 +0100

    remove wayback spider; transfer 2014 BOA scraping to github spider

--- dhscraper/spiders/git_spider.py
+++ dhscraper/spiders/git_spider.py
@@ -1,10 +1,9 @@
 import scrapy
 from dhscraper.items import DhscraperItem
-import xml.etree.ElementTree as ET
+from lxml import etree, html
 import json
-import regex
 import logging
-from ..constants import URL_PATTERN
+from ..utils import extract_urls
 
 
 class GitSpider(scrapy.Spider):
@@ -13,6 +12,7 @@ class GitSpider(scrapy.Spider):
     allowed_domains = ["api.github.com", "raw.githubusercontent.com"]
     start_urls = ["https://api.github.com/repos/ADHO/dh2016/contents/xml",
                   "https://api.github.com/repos/ADHO/dh2015/contents/xml",
+                  "https://api.github.com/repos/elliewix/DHAnalysis/contents/DH2014/abstracts",
                   "https://api.github.com/repos/ADHO/data_dh2013/contents/source/tei",
                   "https://api.github.com/repos/747/tei-to-pdf-dh2022/contents/input/files",
                   "https://api.github.com/repos/ADHO/dh2018/contents/xml/long-papers",
@@ -39,23 +39,33 @@ class GitSpider(scrapy.Spider):
         item = DhscraperItem()
         logging.debug('DhscraperItem created')
         item["origin"] = response.meta["start_url"]
-        xml_string = response.text
-        root = ET.fromstring(xml_string)
-        refs = root.findall('.//{http://www.tei-c.org/ns/1.0}ref')
         item["abstract"] = response.url
+        xml_string = response.body
+
+        if "DH2014" in response.meta["start_url"]:
+            root = html.fromstring(xml_string)
+            try:
+                body_element = root.xpath('//*[contains(@xmlns, "http://www.tei-c.org/ns/1.0")]')[0]
+            except IndexError:
+                body_element = "" # prevent UnboundLocalError: cannot access local variable 'body_element' where it is not associated with a value  in body_text = ''.join...
+                logging.error('No element with xmlns attribute found: Document potentially empty')
+            tag, attr_name = ('a', 'href')
+        else:
+            root = etree.fromstring(xml_string)
+            body_element = root.find('.//{http://www.tei-c.org/ns/1.0}body') # use root.xpath here as well?
+            tag, attr_name = ('ref', 'target')
+
+        refs = root.findall(f'.//{{http://www.tei-c.org/ns/1.0}}{tag}') # refs = root.xpath(f'.//tei:{tag}', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'}) - see if this solves the issua with 2013 abstract 300
         try:
-            item["urls"] = {ref.attrib['target'] for ref in refs}
+            urls = {ref.attrib[attr_name] for ref in refs}
+            item["urls"] = urls
+            logging.debug('Attribute matches found: %s', urls)
         except KeyError:
             logging.error('Ref element does not have a target attribute.')
-        # catch malformed urls: Several urls are not <ref> element attributes, but plain text in <p>, <item> or <bibl>? elements
-        # also search listBibl elements for urls not contained in ref elements?
-        #bibl_text = ' '.join([bibl.text for bibl in root.findall('.//{http://www.tei-c.org/ns/1.0}bibl') if bibl.text])
-        body_element = root.find('.//{http://www.tei-c.org/ns/1.0}body')
+
+        # catch malformed urls: Several urls are not <ref> element attributes, but plain text in <p>, <item> or <bibl> elements
         body_text = ''.join(body_element.itertext())
-        try:
-            mf_urls = {match.group() for match in regex.finditer(URL_PATTERN, body_text, timeout=20)} # body_text + bibl_text
-            logging.debug('Matches found: %s', mf_urls)
-            item["urls"].update(mf_urls)
-        except regex.TimeoutError:
-            logging.error('Regex timed out on %s', response.url)
+        mf_urls = extract_urls(body_text)
+        logging.debug('String matches found: %s', mf_urls)
+        item["urls"].update(mf_urls)
         yield item
