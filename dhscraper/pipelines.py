485c477083463fec4e981e0a23cd828dd8a321a7
Lisa Poggel <poggellisa@gmail.com>
2023-11-15 13:22:43 +0100

    refine url processing in dhscraper pipeline to enhance detection of valid urls

--- dhscraper/pipelines.py
+++ dhscraper/pipelines.py
@@ -34,20 +34,49 @@ class DhscraperPipeline:
                            "theguardian", "sciencedirect", "archives-ouvertes"]
         exclude_subdomains = ["journals"]
         adapter["urls"] = list(adapter["urls"])  # convert set to list
-        pattern_end = r'[\./\):]+$|(\-?\n)|\s|\([^)]+$|\((A|accessed).*|\s*$'  # match any combination of /, ), : and . or open brackets at the end of a sentence, \n and -\n anywhere, with trailing whitespace
+        pattern_end = r'[\./\):]+$|\([^)]+$|\((A|accessed).*|\s*$'  # match any combination of /, ), : and . or open brackets at the end of a sentence, and any trailing whitespace
+        pattern_newline = r'(\-?(\n|\r)|\s)'  # match \n or \r optionally preceded by -, and whitespace anywhere in the url
         valid_urls = []
+
         for url in adapter["urls"]:
+            logging.debug('Current URL is: %s', url)
             cleaned_url = re.sub(pattern_end, "", url)
-            url_parts = extract(cleaned_url)
+            newline_free_url = re.sub(pattern_newline, "", cleaned_url)
+            url_parts = extract(newline_free_url)
+            suffix = url_parts.suffix  # returns "" if no suffix is found
+            if not suffix:  # kein Suffix gefunden
+                matches = list(re.finditer(pattern_newline, cleaned_url))
+                logging.debug('No suffix found in URL: %s', newline_free_url)
+                logging.debug(f'Found {len(matches)} newline characters in URL {cleaned_url}: {matches}')
+                while not suffix and len(matches) > 0:
+                    logging.debug('While loop initiated for: %s', newline_free_url)
+                    # Get and remove the last match
+                    last_match = matches.pop()
+                    # Remove everything after the last match
+                    cleaned_url = cleaned_url[:last_match.start()]
+                    # Update variables url_parts, suffix and newline_free_url
+                    newline_free_url = re.sub(pattern_newline, "", cleaned_url)
+                    logging.debug('Searching suffix for new URL: %s', newline_free_url)
+                    url_parts = extract(newline_free_url)
+                    suffix = url_parts.suffix
+                    if suffix:
+                        logging.debug('Suffix found in URL after removal: %s', newline_free_url)
+                        break
+                    else:
+                        logging.debug('No suffix found in URL after removal: %s', newline_free_url)
+            else:  # Suffix gefunden
+                logging.debug('Suffix found in URL: %s', newline_free_url)
+
             if (
                     url_parts.domain.lower() not in exclude_domains
                     and url_parts.subdomain.lower() not in exclude_subdomains
-                    and not cleaned_url.endswith((".pdf", ".xml", ".jpg", ".jpeg", ".png", ".md", ".txt", ".zip"))
-                    and not cleaned_url.startswith("mailto")
+                    and not newline_free_url.endswith((".pdf", ".xml", ".jpg", ".jpeg", ".png", ".md", ".txt", ".zip"))
+                    and not newline_free_url.startswith("mailto")
                     and url_parts.suffix  # validate url: suffix is empty string if invalid
-                    and not validators.email(cleaned_url)  # exclude email addresses not preceded by mailto scheme
+                    and not validators.email(newline_free_url)  # exclude email addresses not preceded by mailto scheme
             ):
-                valid_urls.append(cleaned_url)
+                valid_urls.append(newline_free_url)
+                logging.debug('URL valid: %s', newline_free_url)
         logging.debug("Cleaned or filtered out URLs: %s", list(set(adapter["urls"]) - set(valid_urls)))
         adapter["urls"] = valid_urls
 
@@ -105,4 +134,5 @@ class GoogleSheetsPipeline:
         header = ["Year", "Origin", "Abstract", "Path Length", "Project Url", "Notes", "HTTP Status"]
         if len(self.sh.get_all_values()) == 0:
             self.sh.append_row(header)
+        logging.debug(f'Appended {len(self.rows)} to Google Sheets.')
         self.sh.append_rows(self.rows)
